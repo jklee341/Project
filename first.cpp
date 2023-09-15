5004bda4f9ee690e983fdd083703d2a0186d87f1
jklee341 <jklee341@gmail.com>
2023-09-15 03:04:21 -0500

    changing first.cpp

--- first.cpp
+++ first.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
-using namespace  std;
+using namespace std;
+
+int square(int a){
+    return a*a;
+}
+
 int main(){
-    cout<<"Hello world";
+    cout << "Hello World\n";
+    for (int i=0; i<9; i++){
+        cout << square(i) << '\n';
+    }
     return 0;
 }
